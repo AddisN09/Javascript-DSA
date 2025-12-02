7408a199d91be23ea8b320b5a9721cf3a91fb79f
Addis Negash <anegash154@gmail.com>
2025-12-02 21:53:35 +0300

    feat: add append operation to singly linked list data Structure

--- src/structures/LInkedlist/singlelinkedlist.js
+++ src/structures/LInkedlist/singlelinkedlist.js
@@ -8,11 +8,29 @@ export class singlyLinkedList{
     constructor(){
         this.head=null;
         this.size=0;
-    }
+    } 
+
      isEmpty(){
         if(!this.head){
             return true;
         }
         return false;
      }
+     append(data){
+        const newNode=new Node(data);
+        let empty=this.isEmpty();
+        if(empty){
+            this.head=newNode;
+        }
+        else{
+            let temp=this.head;
+            while(temp.next){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+        }
+        this.size++;
+        return this;
+     }
+
 }
