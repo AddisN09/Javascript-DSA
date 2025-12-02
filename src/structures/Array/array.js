46a9b8d6d16937e5363e28b937e0d580121227d7
Addis Negash <anegash154@gmail.com>
2025-12-02 14:39:59 +0300

    feat : add insertion sort to ArrayAlgorithm class

--- src/structures/Array/array.js
+++ src/structures/Array/array.js
@@ -7,4 +7,16 @@
         }
         return -1;
     }
+    insertionSort(array){
+        for(let i=1;i<array.length;i++){
+           let temp=array[i];
+           let j=i-1;
+           while(j>=0 && array[j]>temp){
+            array[j+1]=array[j];
+            j--;
+           }
+           array[j+1]=temp;
+        }
+        return array;
+    }
  }
