796c413904148a44ba5169a1a6404d48f8c707a4
Addis Negash <anegash154@gmail.com>
2025-12-05 22:22:00 +0300

    feat: add append operation to doubly linked list data structure

--- src/structures/LInkedlist/DoublyLinkedList.js
+++ src/structures/LInkedlist/DoublyLinkedList.js
@@ -16,4 +16,21 @@ class DoublyLinkedList{
         }
         return false;
     }
+    append(data){
+        const newNode=new Node(data);
+        let empty=this.isEmpty();
+        if(empty){
+            this.head=newNode;
+        }
+        else{
+            let temp=this.head;
+            while(temp.next){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+            newNode.previous=temp;
+        }
+        this.size++;
+        return this;
+    }
 }
