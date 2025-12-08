7361725a03b5ca25abd8ad24bbed0d6acfe37d90
Addis Negash <anegash154@gmail.com>
2025-12-09 01:06:31 +0300

    feat: add isEmpty method to stack data structure

--- src/structures/Stack/ArrayStack.js
+++ src/structures/Stack/ArrayStack.js
@@ -2,4 +2,11 @@ class ArrayStack{
     constructor(){
         this.stack=[];
     }
+    isEmpty(){
+        let length=this.stack.length;
+        if(!length){
+             return true;
+        }
+        return false;
+    }
 }
